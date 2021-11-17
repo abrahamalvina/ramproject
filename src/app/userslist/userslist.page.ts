10e649e4e055ef67762a445f1d1937131f31905d
abrahamalvina <86324493+abrahamalvina@users.noreply.github.com>
2021-11-17 01:29:26 -0300

    Tutorial completo

--- src/app/userslist/userslist.page.ts
+++ src/app/userslist/userslist.page.ts
@@ -8,14 +8,17 @@ import { HttpClient } from '@angular/common/http'
 })
 export class UserslistPage implements OnInit {
 
+  characters = []
+
   constructor(
     private http: HttpClient
   ) { }
 
   ngOnInit() {
-    this.http.get('https://rickandmortyapi.com/api/character')
+    this.http.get<any>('https://rickandmortyapi.com/api/character')
       .subscribe(res => {
         console.log(res);
+        this.characters = res.results;
       })    
   }
 
