03f806763512a2a9b0c10ba26a6185dcf8003185
abrahamalvina <86324493+abrahamalvina@users.noreply.github.com>
2021-11-17 00:35:01 -0300

    Se incorporan import, agrega api al ngOnInit

--- src/app/userslist/userslist.page.ts
+++ src/app/userslist/userslist.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-userslist',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserslistPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
+    this.http.get('https://rickandmortyapi.com/api/character')
+      .subscribe(res => {
+        console.log(res);
+      })    
   }
 
 }
